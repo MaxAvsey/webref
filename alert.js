e73648b9aba153e8cf6c390ed0d89462e9a5968e
MaxAvsey <avseymax@mail.ru>
2022-03-22 20:15:37 +0300

    use new functions alert, prompt, confirm

--- alert.js
+++ alert.js
@@ -1,5 +1,13 @@
-
-let admin, name;
-name = "John"
-admin = name;
-alert(admin);
+'use strict';
+// let admin, name;
+// name = "John"
+// admin = name;
+// // alert(admin);
+// const ourPlanetName = "Earth";
+// const currentUserName = "Maxim";
+// let age = prompt('How old are you?', 100);
+// alert(`You are ${age} years old`);
+// let agreement = confirm('are you agree?');
+// alert(agreement);
+let userName = prompt('What is your name?', 'Name');
+alert(`Hello ${userName}`);
