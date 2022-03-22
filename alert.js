57206158b9f9a2add82a6e6d904f2a2ba9446551
MaxAvsey <avseymax@mail.ru>
2022-03-22 21:28:03 +0300

    ternary operator

--- alert.js
+++ alert.js
@@ -20,11 +20,12 @@
 // } else {
 //     alert("Don't you know?");
 // };
-let value = prompt('Write a number','');
-if (value > 0){
-    alert(1);
-} else if (value < 0) {
-    alert(-1);
-} else {
-    alert(0);
-};
+// let value = prompt('Write a number','');
+// if (value > 0){
+//     alert(1);
+// } else if (value < 0) {
+//     alert(-1);
+// } else {
+//     alert(0);
+// };
+let result = (a + b < 4) ? 'Мало' : 'Много';
