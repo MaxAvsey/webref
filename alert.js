8b01e393b8a58e36dfbbda7fd5c34c8270bf7c37
MaxAvsey <avseymax@mail.ru>
2022-03-25 21:23:13 +0300

    do if else into switch statement

--- alert.js
+++ alert.js
@@ -111,7 +111,6 @@
 // }
 /////////////////////////////////////////////////
 // Напишите if..else, соответствующий следующему switch:
-//
 // switch (browser) {
 //   case 'Edge':
 //     alert( "You've got the Edge!" );
@@ -127,13 +126,47 @@
 //   default:
 //     alert( 'We hope that this page looks ok!' );
 // }
-    if(browser === 'Edge'){
-        alert("You've got the Edge!");
-    } else if(browser === 'Chrome' || browser === 'Firefox' || browser === 'Safari' || browser === 'Opera' ) {
-        alert('Okay we support these browsers too');
-    } else {
-        alert('We hope that this page looks ok!');
-    }
+    // if (browser === 'Edge'){
+    //     alert("You've got the Edge!");
+    // } else if(browser === 'Chrome' 
+    // || browser === 'Firefox' 
+    // || browser === 'Safari' 
+    // || browser === 'Opera' ) {
+    //     alert('Okay we support these browsers too');
+    // } else {
+    //     alert('We hope that this page looks ok!');
+    // }
+    ////////////////////////////////////////////////////////////////
+//     Перепишите код с использованием одной конструкции switch:
+
+// const number = +prompt('Введите число между 0 и 3', '');
+
+// if (number === 0) {
+//   alert('Вы ввели число 0');
+// }
+
+// if (number === 1) {
+//   alert('Вы ввели число 1');
+// }
+
+// if (number === 2 || number === 3) {
+//   alert('Вы ввели число 2, а может и 3');
+// }
+const number = prompt('Enter number','0');
+switch(number) {
+    case '0':
+    alert('You entered 0');
+    break;
+    case '1':
+    alert('You entered 1');
+    break;
+    case '2':
+    case '3':
+    alert('You entered 3 or maybe 2')
+    break;
+    default:
+        alert("I do not know");
+}
 
 
 
