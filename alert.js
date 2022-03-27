cd27c40a6c861854d1ccb8f142f25984c7289643
MaxAvsey <avseymax@mail.ru>
2022-03-27 11:59:00 +0300

    change expression to arrow function

--- alert.js
+++ alert.js
@@ -192,15 +192,60 @@
 
 // min(2, 5) == 2
 // min(3, -1) == -1
-// min(1, 1) == 1
+// // min(1, 1) == 1
 
-function min(a, b){
-    if (a < b) {
-        return a;
-    } else {
-        return b;
-    }
+// function min(a, b){
+//     if (a < b) {
+//         return a;
+//     } else {
+//         return b;
+//     }
+// }
+// function min(a, b) {
+//     return (a < b) ? a : b;
+// }
+//////////////////////////////////////////////////////////////
+// Напишите функцию pow(x,n), которая возвращает x в степени n. Иначе говоря, умножает x на
+// себя n раз и возвращает результат.
+
+// pow(3, 2) = 3 * 3 = 9
+// pow(3, 3) = 3 * 3 * 3 = 27
+// pow(1, 100) = 1 * 1 * ...* 1 = 1
+// function pow(x, n) {
+//     let result = x;
+//     for(let i = 1; i < n; i++){
+//         result *= x;
+//     }
+//     return result;
+// }
+// let x = prompt('x','0')
+// let n = prompt('n','0')
+
+// if (n < 1) {
+//     alert(`Степень ${n} не поддерживается, используйте натуральное число`);
+//   } else {
+//     alert( pow(x, n) );
+//   }
+//Замените код Function Expression стрелочной функцией:
+// Замените код Function Expression стрелочной функцией:
+
+// function ask(question, yes, no) {
+//   if (confirm(question)) yes()
+//   else no();
+// }
+
+// ask(
+//   "Вы согласны?",
+//   function() { alert("Вы согласились."); },
+//   function() { alert("Вы отменили выполнение."); }
+// );
+function ask(question, yes, no) {
+  if (confirm(question)) yes()
+  else no();
 }
-function min(a, b) {
-    return (a < b) ? a : b;
-}
+
+ask(
+  "Вы согласны?",
+  () => alert("Вы согласились."),
+  () => alert("Вы отменили выполнение.")
+);
